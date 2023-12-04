4e13f2a2b39a6811e1c17cf3c216556d4bf076f2
nora-ahmed <77861864+nora-ahmed@users.noreply.github.com>
2023-12-04 23:18:00 +0200

    Merge remote-tracking branch 'origin/main'نتتهن

--- Restaurant.java
+++ Restaurant.java
@@ -7,6 +7,7 @@ public class Restaurant {
     private float ratings;
     private Menu menu;
 
+
     public Restaurant(String name, String address, String category, String contactInfo, float ratings, Menu menu) {
         this.name = name;
         this.address = address;
